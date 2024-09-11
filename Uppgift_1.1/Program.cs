54d5863c6207fdd4ed9ecdec62f1ea4d5a33c26a
ercr07001 <ercr07001@E-PW05CGR0>
2024-09-11 12:25:23 +0200

    Add project files.

---
+++ Uppgift_1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Du är nästan klar med Kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\ "); 
+
+        }
+    }
+}
